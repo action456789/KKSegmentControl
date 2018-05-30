f4acbf712363845e2b81cf01ee9eeaaa67210278
kesen <kesen@youzan.com>
2018-05-30 16:58:16 +0800

    Initial Commit

---
+++ SegmentControl/SegmentControl.h
@@ -0,0 +1,19 @@
+//
+//  SegmentControl.h
+//  SegmentControl
+//
+//  Created by ke sen. on 2018/5/30.
+//  Copyright © 2018年 ke sen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SegmentControl.
+FOUNDATION_EXPORT double SegmentControlVersionNumber;
+
+//! Project version string for SegmentControl.
+FOUNDATION_EXPORT const unsigned char SegmentControlVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SegmentControl/PublicHeader.h>
+
+
